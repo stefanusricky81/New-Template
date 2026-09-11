14c97e7a0be39435b3df116dd690e46083fc3205
stefanusricky81 <stefanus.ricky.81@gmail.com>
2026-09-11 12:20:53 +0700

    Add files via upload

---
+++ calledWebServices/calledWebServices/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+
+namespace calledWebServices
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ServiceReference1.WsRecruitmentSoap a = new ServiceReference1.WsRecruitmentSoapClient();
+            DataTable dt = a.GetEmail();
+            string str = a.HelloWorld();
+
+            for(int i = 0; i < dt.Rows.Count; i++)
+            {
+                listBox1.Items.Add(dt.Rows[i][0].ToString());
+            }
+            listBox1.Items.Add(str);
+        }
+    }
+}
